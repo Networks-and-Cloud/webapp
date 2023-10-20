e1295627f0fcde06125d41bac78d0a8d78020c7e
vidish-kale <kale.v@northeastern.edu>
2023-10-19 20:43:15 -0400

    updating the code

--- test.js
+++ test.js
@@ -8,16 +8,27 @@ use(chaiHttp);
 import app from './app2.js';
 
 describe('API Tests', () => {
-  
-
+   let isError=false;
+  after(()=>{
+    if(isError){
+      // terminate with success
+      process.exit(0);
+    }else{
+      //throw error 
+      process.exit(1);
+    }
+  })
   it('', (done) => {  
     chai.request(app)
       .get('/healthz') // Replace with the actual endpoint you want to test
       .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
+        try {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+        } catch (error) {
+         isError=true;
+        }
         done();
-        process.exit(0);
       });
   });
 
