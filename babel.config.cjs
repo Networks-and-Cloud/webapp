6390afd71b162f52bdd377205e0afe4a672abf5a
vidish-kale <kale.v@northeastern.edu>
2023-11-09 04:17:10 -0500

    Minor changes in PAcker

--- babel.config.cjs
+++
@@ -1,9 +0,0 @@
-// babel.config.js or .babelrc
-
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-    ],
-  ],
-};
--- logger.js
+++ logger.js
@@ -1,55 +1,21 @@
-// const winston = require('winston');
-// const WinstonCloudWatch = require('winston-cloudwatch');
-//import winston from 'winston/lib/winston/config';
-
 import winston from 'winston';
+import { createLogger, format, transports } from 'winston';
 
-import moment from 'moment';
+// Create a logger instance
+const logger = createLogger({
+  level: 'info', // Set the logger block level to info
+  format: format.combine(
+    format.timestamp(),
+    format.json()
 
-//const moment = require('moment');
-
- 
-const currentDate = moment().format('YYYY-MM-DD');
- 
- 
-// Define the log format
-const logFormat = winston.format.combine(
-  winston.format.timestamp(),
-  winston.format.simple()
-);
- 
-// Create a Winston logger with multiple transports for different log levels
-const logger = winston.createLogger({
-  level: 'info', // Minimum log level to capture
-  format: logFormat,
-//   defaultMeta: { service: 'your-service-name' }, // Customize service name
+),
   transports: [
-    // Log 'info' and above messages to a file
-    new winston.transports.File({
-      filename: "/opt/csye6225/webapp/var/log/csye6225.log",
-      level: 'info',
-    }),
- 
-    // Log 'error' and 'warning' messages to a separate file
-    new winston.transports.File({
-      filename: "var/log/csye6225.log",
-      level: 'error',
-    }),
- 
-    // Log 'warning' and above messages to the console
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      ),
-      level: 'warn',
-    }),
-  ],
+    new transports.Console(), // Console transport will log messages at info level or above
+    new transports.File({
+      filename: 'app.log',
+      level: 'info' // File transport will only log messages at error level
+    })
+  ]
 });
- 
-
 
 export default logger;
-// module.exports = logger;
- 
- 
